a6794694216eb929b8d870b04b82a28de907ce53
Renzo Visperas <renzovisperas07@gmail.com>
2022-10-16 21:30:02 +0800

    add react hook form to sign up

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -6,33 +6,84 @@ import {
   Group,
   Anchor,
   Button,
+  Notification,
 } from "@mantine/core";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+
+type FormData = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
 
 const SignUp = () => {
+  const { register, handleSubmit } = useForm<FormData>();
+  const [notification, setNotification] = useState<string | null>(null);
+  const onSubmit = handleSubmit((data) => {
+    const { email, password, confirmPassword } = data;
+
+    if (password !== confirmPassword) {
+      setNotification("Passwords do not match!");
+    }
+
+    console.log(email);
+  });
+
   return (
-    <Container>
-      <Container size="xs" mt={48}>
-        <Paper radius="md" p="xl" withBorder>
-          <Title order={2} weight={500}>
-            Register
-          </Title>
+    <>
+      <Container>
+        <form onSubmit={onSubmit}>
+          <Container size="xs" mt={48}>
+            <Paper radius="md" p="xl" withBorder>
+              <Title order={2} weight={500}>
+                Register
+              </Title>
 
-          <TextInput label="Email" mt="lg" />
-          <TextInput label="Password" mt="sm" />
-          <TextInput label="Confirm Password" mt="sm" />
+              <TextInput
+                label="Email"
+                type="email"
+                mt="lg"
+                {...register("email", { required: true })}
+              />
+              <TextInput
+                label="Password"
+                type="password"
+                mt="sm"
+                {...register("password", { required: true })}
+              />
+              <TextInput
+                label="Confirm Password"
+                type="password"
+                mt="sm"
+                {...register("confirmPassword", { required: true })}
+              />
 
-          <Group position="apart" mt="xl">
-            <Link href="/sign-in" passHref>
-              <Anchor component="a" color="dimmed" size="xs">
-                Already have an account? Login
-              </Anchor>
-            </Link>
-            <Button type="submit">Sign Up</Button>
-          </Group>
-        </Paper>
+              <Group position="apart" mt="xl">
+                <Link href="/sign-in" passHref>
+                  <Anchor component="a" color="dimmed" size="xs">
+                    Already have an account? Login
+                  </Anchor>
+                </Link>
+                <Button type="submit">Sign Up</Button>
+              </Group>
+            </Paper>
+          </Container>
+        </form>
       </Container>
-    </Container>
+      {notification && (
+        <Container size={384}>
+          <Notification
+            color="red"
+            className="notification"
+            onClose={() => setNotification(null)}
+          >
+            Passwords do not match!
+          </Notification>
+        </Container>
+      )}
+    </>
   );
 };
 
