c209534d208e7eae1b088ec0b6714640240002d0
Renzo Visperas <renzovisperas07@gmail.com>
2022-10-19 21:35:51 +0800

    add file upload on avatar click

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,26 +1,70 @@
 import Layout from "../components/Layout/Layout";
-import { Container, Avatar, Group, Title } from "@mantine/core";
+import {
+  Container,
+  Avatar,
+  Group,
+  Title,
+  Notification,
+  Text,
+  Button,
+} from "@mantine/core";
 import withAuthentication from "../hoc/withAuthentication";
 import styles from "../styles/Profile.module.css";
+import { ChangeEvent, useState } from "react";
 
 const Profile = () => {
+  const [avatarPreview, setAvatarPreview] = useState<string | null>(null);
+
+  const handleFileInputChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files && e.target.files[0];
+
+    if (file) {
+      setAvatarPreview(URL.createObjectURL(file));
+    }
+
+    e.target.value = "";
+  };
+
   return (
     <Layout>
       <Container size="xl" className={styles.container}>
         <Container fluid className={styles.background} />
         <Container className={styles.details}>
           <Group spacing="xl">
-            <Avatar
-              src="/images/avatar.jpg"
-              alt="profile picture"
-              radius={100}
-              size={128}
-              className={styles.avatar}
-            />
+            <label htmlFor="profilePic" className={styles.avatar}>
+              <Avatar
+                src={avatarPreview || "/images/avatar.jpg"}
+                alt="profile picture"
+                radius={100}
+                size={128}
+              />
+              <input
+                type="file"
+                accept="image/jpg,image/jpeg,image/png"
+                id="profilePic"
+                hidden
+                onChange={handleFileInputChange}
+              />
+            </label>
+
             <Title>Bowl Cut</Title>
           </Group>
         </Container>
       </Container>
+      {avatarPreview && (
+        <Container size={384}>
+          <Notification
+            color="yellow"
+            className="notification"
+            onClose={() => setAvatarPreview(null)}
+          >
+            <Group>
+              <Text>Detected changes in your profile</Text>
+              <Button>Save</Button>
+            </Group>
+          </Notification>
+        </Container>
+      )}
     </Layout>
   );
 };
