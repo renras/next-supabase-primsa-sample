3e1440b67da5fc680d6521c8e19518a428a26407
Renzo Visperas <renzovisperas07@gmail.com>
2022-10-28 13:53:13 +0800

    add peers table and add modal

--- pages/reviews.tsx
+++ pages/reviews.tsx
@@ -1,13 +1,17 @@
 import Layout from "../components/Layout/Layout";
-import { Container } from "@mantine/core";
+import { Container, Table, Button, Modal, Title } from "@mantine/core";
 import { useState, useEffect } from "react";
-import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+import withAuthentication from "../hoc/withAuthentication";
+import { User } from "../types/User";
 
 const Reviews = () => {
   const supabase = useSupabaseClient();
+  const user = useUser();
   const [usersError, setUsersError] = useState(false);
   const [usersLoading, setUsersLoading] = useState(false);
-  const [users, setUsers] = useState<any>(null);
+  const [users, setUsers] = useState<User[] | null>(null);
+  const [isReviewing, setIsReviewing] = useState(false);
 
   useEffect(() => {
     let mounted = true;
@@ -17,7 +21,7 @@ const Reviews = () => {
 
         if (error) throw error;
 
-        mounted && setUsers(users);
+        mounted && setUsers(users as unknown as User[]);
       } catch (error) {
         console.error(error);
         mounted && setUsersError(true);
@@ -34,13 +38,45 @@ const Reviews = () => {
   if (usersLoading) return <div>Loading...</div>;
   if (usersError) return <div>Error...</div>;
 
-  console.log(users);
+  const peers = users?.filter((peer) => peer.id !== user?.id);
 
   return (
-    <Layout>
-      <Container size="xl">reviews</Container>
-    </Layout>
+    <>
+      <Layout>
+        <Container size="xl">
+          <Title id="peers-table-header" weight="600" mt={64}>
+            Peers
+          </Title>
+          <Table mt="xl" aria-labelledby="peers-table-header">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {peers?.map((peer) => (
+                <tr key={peer.id}>
+                  <td>{peer.email}</td>
+                  <td>
+                    <Button onClick={() => setIsReviewing(true)}>Review</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
+      </Layout>
+
+      <Modal
+        opened={isReviewing}
+        onClose={() => setIsReviewing(false)}
+        title="Evaluate your peer"
+      >
+        Hello
+      </Modal>
+    </>
   );
 };
 
-export default Reviews;
+export default withAuthentication(Reviews);
